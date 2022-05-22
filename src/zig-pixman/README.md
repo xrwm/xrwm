8b8a7b09bef53df56704afa1eb26b82fe10ee670
mbund <25110595+mbund@users.noreply.github.com>
2022-05-21 20:24:03 -0400

    reorganize

--- src/zig-pixman/README.md
+++
@@ -1,5 +0,0 @@
-# zig-pixman
-
-[zig](https://ziglang.org/) bindings for
-[pixman](https://gitlab.freedesktop.org/pixman/pixman) that are a little
-nicer to use than the output of `zig translate-c`.
--- src/zig-wlroots/src/version.zig
+++
@@ -1,9 +0,0 @@
-const c = @cImport(@cInclude("wlr/version.h"));
-
-pub const str = c.WLR_VERSION_STR;
-
-pub const major = c.WLR_VERSION_MAJOR;
-pub const minor = c.WLR_VERSION_MINOR;
-pub const micro = c.WLR_VERSION_MICRO;
-
-pub const num = c.WLR_VERSION_NUM;
